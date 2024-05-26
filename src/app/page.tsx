70e54644287a7b52c0b55e0984c6a45c71026578
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-05-27 03:30:48 +0800

    Finalize design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Forward from "@/components/icons/Forward";
 import Pause from "@/components/icons/Pause";
 import Play from "@/components/icons/Play";
 import Previous from "@/components/icons/Previous";
+import Image from "next/image";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 const SONGDATA = [
@@ -12,7 +13,7 @@ const SONGDATA = [
     title: "Salamin, Salamin",
     artist: "BINI",
     src: "/audios/salamin.mp3",
-    cover: "/images/salamin.jpg",
+    cover: "/images/salamin.webp",
   },
   {
     id: 2,
@@ -34,35 +35,68 @@ type Song = {
 export default function Home() {
   const [active, setActive] = useState(false);
   const [song, setSong] = useState<Song>(SONGDATA[0]);
-  const [audio, setAudio] = useState<string>("");
   const [title, setTitle] = useState<string | undefined>(undefined);
   const [artist, setArtist] = useState<string | undefined>(undefined);
-  const [cover, setCover] = useState<string | undefined>(undefined);
+  const [cover, setCover] = useState<string>("");
   const [position, setPosition] = useState<string | undefined>(undefined);
-  const [time, setTime] = useState<string | undefined>(undefined);
+  const [time, setTime] = useState<string | undefined>("0:00 - 0:00");
   const discStyle = active ? "animate-spin-slow" : "";
 
   let currentSong = useMemo(() => new Audio(), []);
 
+  const playPrevNext = useCallback(
+    (songNumber: number) => {
+      const song = SONGDATA[songNumber];
+      currentSong.src = song.src;
+      setTitle(song.title);
+      setArtist(song.artist);
+      setCover(song.cover);
+      currentSong.play();
+      setActive(true);
+    },
+    [currentSong]
+  );
+
+  const prev = () => {
+    let songNumber = SONGDATA.findIndex((item) => item.id === song.id);
+    songNumber = (songNumber + 1) % SONGDATA.length;
+    songNumber = (songNumber - 1 + SONGDATA.length) % SONGDATA.length;
+    playPrevNext(songNumber);
+  };
+  const next = useCallback(() => {
+    let songNumber = SONGDATA.findIndex((item) => item.id === song.id);
+    songNumber = (songNumber + 1) % SONGDATA.length;
+    playPrevNext(songNumber);
+  }, [playPrevNext, song]);
+
+  const fillBar = document.querySelector(".fill-bar");
+
+  const updateProgress = useCallback(() => {
+    if (currentSong.duration) {
+      setPosition(
+        ((currentSong.currentTime / currentSong.duration) * 100).toFixed(0) +
+          "%"
+      );
+      const duration = formatTime(currentSong.duration);
+      const currentTime = formatTime(currentSong.currentTime);
+
+      setTime(`${currentTime} - ${duration}`);
+    }
+  }, [currentSong]);
+
   const loadSong = useCallback(() => {
-    currentSong.src = audio;
-    setAudio(song.src);
     setTitle(song.title);
     setArtist(song.artist);
-    setCover("bg-[url('" + song.cover + "')]");
-  }, [song, audio, currentSong]);
+    setCover(song.cover);
+    currentSong.src = song.src;
+    currentSong.addEventListener("timeupdate", updateProgress);
+    currentSong.addEventListener("ended", next);
+  }, [song, currentSong, next, updateProgress]);
 
   useEffect(() => {
     loadSong();
   }, [loadSong]);
 
-  useEffect(() => {
-    loadSong();
-  }, [song, loadSong]);
-
-  const timeUpdate = () => {};
-  const prev = () => {};
-  const next = () => {};
   const toggle = () => {
     if (active) {
       currentSong.pause();
@@ -72,50 +106,47 @@ export default function Home() {
 
     setActive(!active);
   };
-  const seek = () => {};
+
   const formatTime = (time: number) => {
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
 
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
-  const updateProgress = () => {
-    if (currentSong.duration) {
-      setPosition(
-        (
-          "w-[" +
-          (currentSong.currentTime / currentSong.duration) * 100
-        ).toString() + "%]"
-      );
-      const duration = formatTime(currentSong.duration);
-      const currentTime = formatTime(currentSong.currentTime);
 
-      setTime(`${currentTime} - ${duration}`);
-    }
-  };
+  // todo: Seek functionality refactor next and prev
 
   return (
-    <main className="flex min-h-screen bg-[#ddd]">
+    <main className="flex min-h-screen bg-[#222831]">
       <div className="flex flex-col text-black justify-center mx-auto p-2 relative">
         <div className="bg-black my-0 mx-4 pt-1 pr-4 pb-1 pl-36 rounded-t-2xl">
           <div className="text-white font-bold text-lg">{title}</div>
           <div className="text-[#777] text-sm mx-0 mt-1 mb-5">{artist}</div>
-          <div className="bg-[#555] rounded-3xl cursor-pointer">
-            <div className={`h-1 rounded-3xl bg-green-600 w-0`}></div>
+          <div className={`bg-[#555] rounded-3xl`}>
+            <div
+              style={{ width: position }}
+              className={`fill-bar h-1 rounded-3xl bg-green-600 w-[0%]`}
+            ></div>
           </div>
-          <div className="text-[#777] text-base my-1">0:00 - 0:00</div>
+          <div className="text-[#777] text-base my-1">{time}</div>
         </div>
         <div className="max-w-36">
           <div className="absolute h-5 w-5 bg-white rounded-full top-[44%] left-20 z-10"></div>
-          <div
-            className={
-              `absolute h-36 w-36 top-[36%] left-4 bg-[url('/images/salamin.jpg')] bg-no-repeat bg-cover bg-bottom border-2 border-white rounded-full shadow-disc transition-all ease-in-out duration-200 ${discStyle} ` +
-              cover
-            }
-          ></div>
+          <Image
+            src={cover}
+            alt="Song cover"
+            width={145}
+            height={145}
+            quality={100}
+            priority={true}
+            className={`absolute h-36 w-36 top-[36%] left-4 border-2 border-white rounded-full transition-all ease-in-out duration-200 bg-cover ${discStyle}`}
+          />
         </div>
-        <div className="flex flex-row justify-center items-center bg-[#333] gap-2 w-96 h-20 rounded-[18px]">
-          <span className="text-[#888] transition-all ease-in duration-300 hover:text-white">
+        <div className="flex flex-row justify-center items-center bg-[#111] gap-2 w-96 h-20 rounded-[18px]">
+          <span
+            onClick={prev}
+            className="text-[#888] transition-all ease-in duration-300 hover:text-white cursor-pointer"
+          >
             <Previous />
           </span>
           <span
@@ -123,11 +154,14 @@ export default function Home() {
               toggle();
               setActive(!active);
             }}
-            className="bg-green-700 text-white p-4 rounded-full transition-all ease-in duration-300 hover:bg-green-800"
+            className="bg-green-700 text-white p-4 rounded-full transition-all ease-in duration-300 hover:bg-green-600 cursor-pointer"
           >
             {active ? <Pause /> : <Play />}
           </span>
-          <span className="text-[#888] transition-all ease-in duration-300 hover:text-white">
+          <span
+            onClick={next}
+            className="text-[#888] transition-all ease-in duration-300 hover:text-white cursor-pointer"
+          >
             <Forward />
           </span>
         </div>
